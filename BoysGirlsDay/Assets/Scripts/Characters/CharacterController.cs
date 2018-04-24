94297bad8ea51d661c9f5ccb017e87e0b444c27e
Patrick Henschel <patrick.henschel@xyrality.com>
2018-04-24 17:21:41 +0200

    inputs

--- BoysGirlsDay/Assets/Scripts/Characters/CharacterController.cs
+++ BoysGirlsDay/Assets/Scripts/Characters/CharacterController.cs
@@ -2,5 +2,25 @@
 
 public class CharacterController : MonoBehaviour 
 {
-    
+
+
+    private void OnGUI()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("A pressed");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("S pressed");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("D pressed");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("W pressed");
+        }
+    }
 }
