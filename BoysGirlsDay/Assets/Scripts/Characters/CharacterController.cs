2ba52139f4531cb89f5d6e669782de30123a5573
Patrick Henschel <patrick.henschel@xyrality.com>
2018-04-25 15:28:01 +0200

    added lower arms to character rig added attack left animation

--- BoysGirlsDay/Assets/Scripts/Characters/CharacterController.cs
+++ BoysGirlsDay/Assets/Scripts/Characters/CharacterController.cs
@@ -1,26 +1,31 @@
-﻿using UnityEngine;
+﻿using DragonBones;
+using UnityEngine;
 
-public class CharacterController : MonoBehaviour 
+public class CharacterController : MonoBehaviour
 {
-
+    [SerializeField] private UnityArmatureComponent character;
 
     private void OnGUI()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("A pressed");
+            //Debug.Log("A pressed");
+            if (character != null) character.animation.Play("idle");
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Debug.Log("S pressed");
+            //Debug.Log("S pressed");
+            if (character != null) character.animation.Play("idle");
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Debug.Log("D pressed");
+            //Debug.Log("D pressed");
+            if (character != null) character.animation.Play("idle");
         }
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("W pressed");
+            //Debug.Log("W pressed");
+            if (character != null) character.animation.Play("idle");
         }
     }
 }
